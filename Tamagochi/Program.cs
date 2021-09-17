d54d9bd15b07c82944011380f8fabbae690f8c7e
EDVIN TSYGANKOV <70744431+PoseironDire@users.noreply.github.com>
2021-09-17 14:05:41 +0200

    Help From Markus

--- Tamagochi/Program.cs
+++ Tamagochi/Program.cs
@@ -5,17 +5,95 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Tamagochi tamagochi = new Tamagochi();
-        bool nameGiven = false;
-        while (!nameGiven)
-        {
-            Console.WriteLine("What Is YOUR Tamagochi Called?");
-            tamagochi.name = Console.ReadLine();
+        //Tamagochis
+        List<Tamagochi> players = new List<Tamagochi>();
+        players = CreateNew(players);
+        Console.ReadLine();
+        Console.Clear();
+        bool playing = true;
 
-            if (tamagochi.name == "2")
+        while (playing)
+        {
+            for (int i = players.Count - 1; i >= 0; i--)
             {
-                Console.WriteLine("gay");
+                System.Console.WriteLine(players.Count);
+                if (players[i].GetAlive())
+                {
+                    players[i].PrintStats();
+
+                    Console.ReadLine();
+
+                    Console.WriteLine("What do you want to do with " + players[i].name + "?:");
+                    Console.WriteLine("1. Teach it");
+                    Console.WriteLine("2. Greet it");
+                    Console.WriteLine("3. Feed it");
+                    Console.WriteLine("4. Breed Tamagochi");
+                    Console.WriteLine("5. Nothing");
+
+                    string Choice = Console.ReadLine().ToLower();
+                    if (Choice == "1" || Choice == "teach it" || Choice == "teach")
+                    {
+                        System.Console.WriteLine("What word do you want to teach " + players[i].name + "?");
+                        players[i].Teach(Console.ReadLine());
+                        Console.WriteLine(players[i].name + " was taught: '" + players[i].words[players[i].words.Count - 1] + "'");
+                    }
+                    else if (Choice == "2" || Choice == "greet it" || Choice == "greet")
+                    {
+                        players[i].Hi();
+                    }
+                    else if (Choice == "3" || Choice == "feed it" || Choice == "feed")
+                    {
+                        players[i].Feed();
+                    }
+                    else if (Choice == "4" || Choice == "breed tamagochi" || Choice == "breed")
+                    {
+                        players = CreateNew(players);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing Happened!");
+                        Console.WriteLine("");
+                    }
+                    players[i].Tick();
+
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                players.RemoveAll(t => !t.GetAlive());
+                if (players.Count <= 0)
+                {
+                    Console.WriteLine("You messed up! All Tamagocis are dead! Do you want to retry?");
+                    string retry = Console.ReadLine().ToLower();
+                    if (retry == "yes")
+                    {
+                        players = CreateNew(players);
+                    }
+                    else
+                    {
+                        playing = false;
+                    }
+                }
             }
         }
+        foreach (Tamagochi item in players)
+        {
+            item.PrintStats();
+            for (int i = 0; i < players.Count - 1; i++)
+            {
+                Console.WriteLine("");
+            }
+        }
+        Console.WriteLine("Game Over!");
+        Console.ReadLine();
+    }
+    //Breed a Tamagochi
+    static List<Tamagochi> CreateNew(List<Tamagochi> tamagochis)
+    {
+        Tamagochi tamagochi = new Tamagochi();
+        Console.WriteLine("What Is YOUR Tamagochi Called?");
+        tamagochi.name = Console.ReadLine();
+        Console.WriteLine("Your Tamagochi's name is: " + tamagochi.name);
+        tamagochis.Add(tamagochi);
+        return tamagochis;
     }
 }
--- Tamagochi/Tamagochi.cs
+++ Tamagochi/Tamagochi.cs
@@ -4,16 +4,16 @@ using System.Collections.Generic;
 public class Tamagochi
 {
     //Tamagochi's Hunger Meter
-    public int hunger;
+    public int hunger = 0;
     //Tamagochi's Hunger Meter
-    public int boredom;
+    public int boredom = 0;
     //Tamagochi's Name 
     public string name;
     //Tamagochi's Words
     public List<string> words = new List<string>();
     public Random generator = new Random();
-    //Tamagochi Dead or Alive?
-    public bool isAlive;
+    //Tamagochi Dead or Alive
+    bool isAlive = true;
 
     // Increase Hunger & Boredom by 1 each Tick, & If Any them reach over 10 isALive = false
     public void Tick()
@@ -21,7 +21,7 @@ public class Tamagochi
         hunger++;
         boredom++;
 
-        if (hunger >= 10 || boredom >= 10)
+        if (hunger > 10 || boredom > 10)
         {
             isAlive = false;
         }
@@ -31,35 +31,73 @@ public class Tamagochi
     public void Feed()
     {
         hunger--;
+        Console.WriteLine(name + " was fed!");
     }
 
-    // skriver ut ett slumpat ord från words, och anropar ReduceBoredom.
+    //Decreases boredom by 1.
+    public void ReduceBoredom()
+    {
+        boredom -= 1;
+    }
+
+    // Writes a random word from words, & runs ReduceBoredom.
     public void Hi()
     {
-
+        int randomWord = generator.Next(words.Count);
+        if (words.Count != 0)
+        {
+            Console.WriteLine(words[randomWord]);
+        }
+        else
+        {
+            System.Console.WriteLine(name + " doesn't know how to speak!");
+        }
+        ReduceBoredom();
     }
 
-    // lägger till ett ord i words, och anropar ReduceBoredom.
+    // Add a word to words, & runs ReduceBoredom.
     public void Teach(string word)
     {
-
+        words.Add(word);
+        ReduceBoredom();
     }
 
     // skriver ut nuvarande hunger och bredom, och meddelar också huruvida tamagotchin lever.
     public void PrintStats()
     {
-
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine(name + " Status:");
+        if (hunger >= 10)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            if (hunger == 10)
+            {
+                Console.WriteLine(name + " is Starving!");
+            }
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        Console.WriteLine("Hunger: " + hunger);
+        if (boredom >= 10)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            if (boredom == 10)
+            {
+                Console.WriteLine(name + " is Depressed!");
+            }
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        Console.WriteLine("Boredom: " + boredom);
+        Console.ForegroundColor = ConsoleColor.White;
     }
-
     // returnerar värdet som isAlive har.
-    public void GetAlive()
+    public bool GetAlive()
     {
-
-    }
-
-    //Decreases boredom.
-    public void ReduceBoredom()
-    {
-        boredom--;
+        return isAlive;
     }
 }
